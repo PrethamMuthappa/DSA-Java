1b8e8672f9466f4ab297b6ae0ea572d949654333
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-08-23 23:32:43 +0530

    Add files via upload

--- stacks/src/balenced_parenthesis/Parenthisis.java
+++ stacks/src/balenced_parenthesis/Parenthisis.java
@@ -1,17 +1,38 @@
 package balenced_parenthesis;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Parenthisis {
 
 	public static void main(String[] args) {
 	
-
+		checks cc=new checks();
+		cc.checking();
+ 
 	}
 
 }
 
 class checks{
-	
+	Stack<String> stack=new Stack<String>();
+	Scanner sc=new Scanner(System.in);
 	public void checking() {
+		
+	System.out.println("enter patterns");
+	String pattern=sc.nextLine();
+	
+	for(int i=0;i<pattern.length();i++) {
+		
+		if(pattern=="(") {
+			
+			stack.push("(");
+			
+		}
+		
+		System.out.println(stack);
+		
+	}
 		
 	}
 }
