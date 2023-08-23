d2c91c42c7f5c4daf9795cf0e0dff1d80071c1e0
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-08-24 00:13:44 +0530

    Add files via upload

--- stacks/src/balenced_parenthesis/Parenthisis.java
+++ stacks/src/balenced_parenthesis/Parenthisis.java
@@ -15,24 +15,56 @@ public class Parenthisis {
 }
 
 class checks{
-	Stack<String> stack=new Stack<String>();
+	Stack<Character> stack=new Stack<Character>();
 	Scanner sc=new Scanner(System.in);
 	public void checking() {
 		
 	System.out.println("enter patterns");
 	String pattern=sc.nextLine();
+		
+    for(char pat :pattern.toCharArray()) {
+    	
+    	if(pat=='{' || pat=='[' || pat==')') {
+    		
+    		stack.push(pat);
+    		
+    	}
+    	else if(pat=='}' || pat==']' || pat==')') {
+    		
+    		if(stack.isEmpty()) {
+    			System.out.println("unbalenced parenthisis");
+    			return;
+    		}else {
+    		
+    		char popped=stack.pop();
+    		
+    		if(!bracketsreturn(popped,pat)) {
+    			System.out.println("unbalenced parenthisis");
+    			
+    			return;
+    		}
+    		
+    		}
+    		
+    		
+    		
+    	}
+    	
+    }
+    
+    if(stack.isEmpty()) {
+    	
+    	System.out.println("balenced");
+    }
+    else {
+    	System.out.println("unbalenced parenthisis");
+    }
+		
+		
 	
-	for(int i=0;i<pattern.length();i++) {
-		
-		if(pattern=="(") {
-			
-			stack.push("(");
-			
-		}
-		
-		System.out.println(stack);
-		
 	}
+	private boolean bracketsreturn(char open, char close) {
 		
+		return (open == '(' && close == ')') || (open == '[' && close == ']') || (open == '{' && close == '}');
 	}
 }
