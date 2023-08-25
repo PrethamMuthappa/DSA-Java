2946e9bd650e0d5a880ab383807b3ea44be988d4
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-08-25 22:50:21 +0530

    Add files via upload

--- stacks/src/reverse_a_string/Reverse.java
+++ stacks/src/reverse_a_string/Reverse.java
@@ -1,10 +1,43 @@
 package reverse_a_string;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Reverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		rev rr=new rev();
+		rr.stringrev();
+ 
 	}
 
+}
+
+
+class rev{
+	
+	Stack<String> stack=new Stack<String>();
+	Scanner sc=new Scanner(System.in);
+	
+	public void stringrev() {
+		System.out.println("ENTER YOUR STRING \n");
+		String ch=sc.nextLine();
+		
+		for(int i=0;i<ch.length();i++) {
+			
+			String abc=String.valueOf(ch.charAt(i));
+			
+			stack.push(abc);
+		}
+		
+		System.out.println(stack);
+		
+		for(int j=0;j<stack.size();i--) {
+			
+		}
+		
+	
+			
+		
+	}
 }
