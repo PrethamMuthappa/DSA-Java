a80185edc696e37c152f8166b0db9b328bb70eb0
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-09-01 21:04:29 +0530

    Add files via upload

--- stacks/src/Simplify_Path_Algorithm/Filepath.java
+++ stacks/src/Simplify_Path_Algorithm/Filepath.java
@@ -24,6 +24,33 @@ class unixfile{
 		System.out.println("ENTER YOUR FILE PATH");
 		String path=sc.nextLine();
 		
+   String[] simppath=path.split("/");
+   
+   for(String i : simppath) {
+	   if(i.equals("")|| i.equals(".")) {
+		   continue;
+	   }
+	   else if(i.equals("..")){
+		   if(!stack.isEmpty()) {
+			   stack.pop();
+		   }
+	   }else {
+		   stack.push(i);
+	   }
+   }
+   
+   StringBuilder strngbuilds=new StringBuilder();
+   for(String dir : stack) {
+	   strngbuilds.append("/");
+	   strngbuilds.append(dir);
+	   
+   }
+   if(strngbuilds.length()==0) {
+	   strngbuilds.append("/");
+   }
+   System.out.println("Simplified Path: " + strngbuilds.toString());
+		
+		
 		
 	}
 }
