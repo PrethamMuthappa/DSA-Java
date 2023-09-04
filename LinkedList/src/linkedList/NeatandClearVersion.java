f5671f898b7b633daaab050d9ff8e5ae6dc13598
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-09-04 23:00:40 +0530

    Update NeatandClearVersion.java

--- LinkedList/src/linkedList/NeatandClearVersion.java
+++ LinkedList/src/linkedList/NeatandClearVersion.java
@@ -1,99 +1,98 @@
-// Node class to represent individual elements in the linked list
-class Node {
-    int data;
-    Node next;
+package test;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
+import java.util.Scanner;
+
+public class Links {
+   
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		inserting ins=new inserting();
+		
+		for(;;) {
+			System.out.println("1 INSERT DATA 2 DISPLAY 3 DELETE 4 EXIT \n");
+			int ch=sc.nextInt();
+			switch(ch) {
+			case 1:System.out.println("ENTER DATA \n");
+			int data=sc.nextInt();
+			ins.add(data);
+			break;
+			case 2:ins.display();
+			break;
+			case 3:System.out.println("ENTER DATA TO DELETE \n");
+			int del=sc.nextInt();
+			ins.delete(del);
+		    break;
+			case 4:System.exit(0);
+			sc.close();
+			Runtime.getRuntime().gc();
+			}
+		}
+	}
+ 
 }
 
-// LinkedList class to manage the list
-class LinkedList {
-    private Node head;
-
-    public LinkedList() {
-        head = null;
-    }
-
-    // Insert a new element at the end of the linked list
-    public void append(int data) {
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-
-        current.next = newNode;
-    }
-
-    // Display the elements of the linked list
-    public void display() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
-            current = current.next;
-        }
-        System.out.println("null");
-    }
-
-    // Search for a specific element in the linked list
-    public boolean search(int data) {
-        Node current = head;
-        while (current != null) {
-            if (current.data == data) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    // Delete a specific element from the linked list
-    public void delete(int data) {
-        if (head == null) {
-            return;
-        }
-
-        if (head.data == data) {
-            head = head.next;
-            return;
-        }
-
-        Node current = head;
-        while (current.next != null) {
-            if (current.next.data == data) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
-        }
-    }
+class Node{
+	int data;
+	Node next;
+	public Node(int data) {
+		this.data=data;
+		this.next=null;
+	}
 }
 
-public class Main {
-    public static void main(String[] args) {
-        LinkedList myList = new LinkedList();
-
-        myList.append(1);
-        myList.append(2);
-        myList.append(3);
-
-        System.out.println("Linked List:");
-        myList.display();
-
-        System.out.println("Search for 2: " + myList.search(2));
-        System.out.println("Search for 4: " + myList.search(4));
-
-        myList.delete(2);
-        System.out.println("Linked List after deleting 2:");
-        myList.display();
-    }
+class inserting{
+	Node head;
+	void add(int element) {
+		
+		Node temp=new Node(element);
+		
+		if(head==null) {
+			
+			head=temp;
+		}
+		else {
+			Node current =head;
+			while(current.next!=null) {
+				current=current.next;
+				
+			}
+			current.next=temp;
+		}
+	};
+	void display() {	
+		Node current=this.head;
+		if(current==null) {
+			System.out.println("EMPTY LIST");
+			return;
+		}
+		while(current!=null) {
+			
+			System.out.println(current.data);
+			current=current.next;
+		}
+	}
+	
+	void delete(int element) {
+		 	
+		if(head==null) {
+			return;
+		}
+		
+		if(head.data==element) {
+			head=head.next;
+		}
+		
+		Node current=head;
+		
+		
+		while(current!=null) {
+			
+		
+		if(current.next.data==element) {
+			current.next=current.next.next;
+			return;
+		}
+		current=current.next;
+		}
+	}
 }
