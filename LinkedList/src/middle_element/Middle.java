968f40f4caff4ce46fb1d92f813afabb9dca94c6
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2023-09-07 00:18:52 +0530

    Update Middle.java

--- LinkedList/src/middle_element/Middle.java
+++ LinkedList/src/middle_element/Middle.java
@@ -16,24 +16,31 @@ public class Middle {
 
 class finds{
 	LinkedList<Integer>ll=new LinkedList<Integer>();
-	Iterator<Integer>slow=ll.iterator();
-	Iterator<Integer>fast=ll.iterator();
 	Scanner sc=new Scanner(System.in);
 	
 	void ML() {
-		System.out.println("enter elements \n");
+		System.out.println("enter no of elements \n");
+		int n=sc.nextInt();
+		System.out.println("enter the elements \n");
 		
-		for(int i=0;i<6;i++) {
+		for(int i=0;i<n;i++) {
 			int data=sc.nextInt();
 			ll.add(data);
 		}
+		Iterator<Integer>slow=ll.iterator();
+		Iterator<Integer>fast=ll.iterator();
 		
-		while(slow.hasNext() && fast.hasNext()) {
-			if(fast==null) {
-				System.out.println(slow);
-				return;
+		while(fast.hasNext() && fast.next()!=null) {
+			fast.next();
+			if(fast.hasNext()) {
+				fast.next();
 			}
+			slow.next();
+			
 		}
+		
+		int middle=slow.next();
+		System.out.println(middle);
 		
 	}
 }
